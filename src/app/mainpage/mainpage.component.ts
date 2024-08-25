2a8c975cd090aaa09e5364089604a5c2519ef11b
Walter Doni <walter.doni@gmx.at>
2024-08-25 21:15:21 +0200

    structur update

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -4,12 +4,14 @@ import { LandingpageComponent } from '../landingpage/landingpage.component';
 import { AboutMeComponent } from '../about-me/about-me.component';
 import { StackComponent } from '../stack/stack.component';
 import { ProjectsComponent } from '../projects/projects.component';
+import { ContactComponent } from '../contact/contact.component';
+import { FooterComponent } from '../footer/footer.component';
 
 
 @Component({
   selector: 'app-mainpage',
   standalone: true,
-  imports: [HeaderComponent, LandingpageComponent, AboutMeComponent, StackComponent,ProjectsComponent],
+  imports: [HeaderComponent, LandingpageComponent, AboutMeComponent, StackComponent,ProjectsComponent, ContactComponent, FooterComponent],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.scss'
 })
