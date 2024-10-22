7077bb05630007b6ce46812ae87a9bac576f8cdc
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-10-22 17:19:44 +0200

    loading animation progress

--- src/app/stack/stack.component.ts
+++ src/app/stack/stack.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-stack',
@@ -9,7 +9,11 @@ import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 })
 export class StackComponent implements AfterViewInit {
   @ViewChild('infoText') infoText!: ElementRef;
+  @ViewChild('imgFrontend') imgFrontend!: ElementRef;
+  @ViewChild('imgBackend') imgBackend!: ElementRef;
+
   InfotextVisible = false;
+  ImgCertificate = false;
 
   ngAfterViewInit() {
     const observer = new IntersectionObserver(entries => {
@@ -18,16 +22,27 @@ export class StackComponent implements AfterViewInit {
           this.showText(); // Funktion aufrufen, um den Text anzuzeigen
           observer.unobserve(entry.target); // Stoppe die Überwachung, nachdem der Text sichtbar ist
         }
+        if (entry.isIntersecting && !this.ImgCertificate) {
+          this.loadingIMG();
+          observer.unobserve(entry.target);
+        }
       });
     });
 
     observer.observe(this.infoText.nativeElement); // Überwache das infoText-Element
+    observer.observe(this.imgFrontend.nativeElement);
   }
+
+  private loadingIMG() {
+    this.imgFrontend.nativeElement.style.transform = "rotate(0deg) translateX(0)";
+    this.imgBackend.nativeElement.style.transform = "rotate(0deg) translatex(0)";
+  }
+
 
   private showText() {
     const paragraph = this.infoText.nativeElement.querySelector('p');
     this.infoText.nativeElement.style.opacity = '1';
-    this.infoText.nativeElement.style.transform = 'translateX(0%)';
+    this.infoText.nativeElement.style.transform = 'translateY(0px)';
     paragraph.style.opacity = '1';
     paragraph.style.transform = 'translateX(0%)';
     this.InfotextVisible = true;
