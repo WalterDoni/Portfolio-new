5ab87c9325bb952d826759a479f3555c32fb5988
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-10-22 16:56:24 +0200

    start with loading animations

--- src/app/stack/stack.component.ts
+++ src/app/stack/stack.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-stack',
   standalone: true,
   imports: [],
   templateUrl: './stack.component.html',
-  styleUrl: './stack.component.scss'
+  styleUrls: ['./stack.component.scss']
 })
-export class StackComponent {
+export class StackComponent implements AfterViewInit {
+  @ViewChild('infoText') infoText!: ElementRef;
+  InfotextVisible = false;
 
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting && !this.InfotextVisible) {
+          this.showText(); // Funktion aufrufen, um den Text anzuzeigen
+          observer.unobserve(entry.target); // Stoppe die Überwachung, nachdem der Text sichtbar ist
+        }
+      });
+    });
+
+    observer.observe(this.infoText.nativeElement); // Überwache das infoText-Element
+  }
+
+  private showText() {
+    const paragraph = this.infoText.nativeElement.querySelector('p');
+    this.infoText.nativeElement.style.opacity = '1';
+    this.infoText.nativeElement.style.transform = 'translateX(0%)';
+    paragraph.style.opacity = '1';
+    paragraph.style.transform = 'translateX(0%)';
+    this.InfotextVisible = true;
+  }
 }
