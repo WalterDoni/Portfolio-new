898f4048d588def1e4207845906edb64fe6ba9d3
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-10-24 14:03:19 +0200

    progress with animation

--- src/app/stack/stack.component.ts
+++ src/app/stack/stack.component.ts
@@ -12,39 +12,36 @@ export class StackComponent implements AfterViewInit {
   @ViewChild('imgFrontend') imgFrontend!: ElementRef;
   @ViewChild('imgBackend') imgBackend!: ElementRef;
 
-  InfotextVisible = false;
-  ImgCertificate = false;
-
   ngAfterViewInit() {
     const observer = new IntersectionObserver(entries => {
       entries.forEach(entry => {
-        if (entry.isIntersecting && !this.InfotextVisible) {
-          this.showText(); // Funktion aufrufen, um den Text anzuzeigen
+        if (entry.isIntersecting) {
+          this.showText();
           observer.unobserve(entry.target); // Stoppe die Überwachung, nachdem der Text sichtbar ist
         }
-        if (entry.isIntersecting && !this.ImgCertificate) {
+        if (entry.isIntersecting) {
           this.loadingIMG();
           observer.unobserve(entry.target);
         }
       });
     });
-
     observer.observe(this.infoText.nativeElement); // Überwache das infoText-Element
     observer.observe(this.imgFrontend.nativeElement);
   }
 
-  private loadingIMG() {
+
+  loadingIMG() {
     this.imgFrontend.nativeElement.style.transform = "rotate(0deg) translateX(0)";
     this.imgBackend.nativeElement.style.transform = "rotate(0deg) translatex(0)";
   }
 
 
-  private showText() {
+  showText() {
     const paragraph = this.infoText.nativeElement.querySelector('p');
     this.infoText.nativeElement.style.opacity = '1';
     this.infoText.nativeElement.style.transform = 'translateY(0px)';
     paragraph.style.opacity = '1';
     paragraph.style.transform = 'translateX(0%)';
-    this.InfotextVisible = true;
+
   }
 }
