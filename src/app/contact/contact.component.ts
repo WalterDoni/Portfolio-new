8455acdc5b4b9509ee2ddf60b6816260cb2f68d6
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-12-01 14:53:45 +0100

    email send

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  name: string = '';
+  email: string = '';
+  message: string = '';
+
+
+  constructor(private http: HttpClient) { }
+
+  async sendMail() {
+    const mailValues = {
+      name: this.name,
+      email: this.email,
+      message: this.message
+    };
+
+    try {
+      const response = await firstValueFrom(this.http.post('ADRESSE EINFÜGEN', mailValues));
+      this.clearValuesFromFormForEmail();
+      alert('Nachricht gesendet')
+    } catch (error) {
+      console.error('Fehler beim Senden der Email', error);
+    }
+  }
+
+  clearValuesFromFormForEmail() {
+    this.name = '';
+    this.email = '';
+    this.message = '';
+  }
+
 
 }
